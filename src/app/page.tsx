3fb3c4e7169bc01bdfe12a4c50c7f3c0b09c3806
anik <tusarimrananik@gmail.com>
2025-06-20 15:58:44 +0600

    refactor: clean up whitespace in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,8 @@ import GeoIpSync from "@/components/GeoIpSync";
 import ResidentialIpPayment from "@/components/ResidentialIpPayment";
 import VpnRequest from "@/components/VpnRequest";
 import multipleStep from '@/lib/multipleStep'
-
 export default async function Home() {
-
   const data = await multipleStep();
-
 
   return (
     <>
